c0fb142b5f3125437cf500e75d3e101ef4242cac
Dhanshree Bhosale <dbhosale00001001@gmail.com>
2024-08-19 11:47:40 +0530

    Add files via upload

---
+++ Calculator/script.js
@@ -0,0 +1,31 @@
+let string = ""
+let buttons =
+document.querySelector(".buttons").children
+console.log(buttons)
+Array.from(buttons).forEach((btns) => {
+    btns.addEventListener('click', (e) => {
+
+
+
+        switch (e.target.innerHTML) {
+            case '=':
+                string = eval(string)
+
+    document.getElementById("display").value = string
+
+
+    break;
+    case 'cl':
+
+    string = ""
+    break;
+
+    default:
+        console.log(e.target);
+        string = string + e.target.innerHTML
+        break;
+        }
+        document.getElementById("display").value =
+        string
+    })
+})
